489c34d30f6eeaae20624c37988b67fd5b7c0146
maimai84 <maialmasalmah@gmail.com>
2017-09-23 09:11:08 +0300

    express

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -5,9 +5,31 @@ var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage
 
 var User = sequelize.define('User', {
   username: Sequelize.STRING
+   userid: Sequelize.number
 });
+var massege = sequelize.define('massege', {
+  massege: Sequelize.STRING,
+   text: Sequelize.STRING,
+   roomname:Sequelize.STRING
+});
+User hasmany massages
+massege belong to user
+user sync();
+massege sync();
+sequelize.sync()
+  .then(() => User.create({
+    username: '',
+    userid: increment id
+  }))
+  .then( => {
+    (get({
+      plain: true
+    }));
+  });
 
 module.exports = { 
   app: app,
   User: User
 };
+
+

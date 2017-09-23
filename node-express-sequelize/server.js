8471de286b4db9e5bf120ae47e5da95f48b6f079
Roqaya Qazala <roukamohammad@gmail.com>
2017-09-23 08:56:30 +0300

    Express

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -7,6 +7,16 @@ var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
 
+////'/users' route that responds to a GET request and
+///* [ ] Uses the provided Sequelize `User` model to send all `users` currently in the database
+
+app.get('/users', function(req,res){
+	User.findAll().then(function(users){
+		res.status(200).send(users);
+		console.log('it is done');
+	})
+});
+
 module.exports = { 
   app: app,
   User: User

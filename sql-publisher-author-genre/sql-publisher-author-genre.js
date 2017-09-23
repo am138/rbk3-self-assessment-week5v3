01012808265e78fd7681a8f4246f9e5f5be9f6b9
maramshon <mralshon@yahoo.com>
2017-09-23 09:30:30 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -10,10 +10,28 @@ var Schemas = {
 };
 
 var sqlPublisherAuthorSchema = 
+//##### Option 6 (below)
+//![pub_auth_option_6](https://cloud.githubusercontent.com/assets/7968370/17832190/d9434962-66b1-11e6-9caa-ba97d3c1647d.png)
+
 // TODO: Write a comment here explaining your choice
 
-var sqlPublisherGenreSchema = 
-// TODO: Write a comment here explaining your choice
+//it is many to many
+
+var sqlPublisherGenreSchema =  
+//##### Option 4 (below)
+//![pub_gen_option_4](https://cloud.githubusercontent.com/assets/7968370/17832208/4b42819a-66b2-11e6-83c4-7346027f4fcb.png)
+
+// TODO: Write a comment here explaining your choice 
+//publisher belonge to genre
+//genre has many publisher
 
 var sqlAuthorGenreSchema = 
+
+// ##### Option 6 (below)
+// ![auth_gen_option_6](https://cloud.githubusercontent.com/assets/7968370/17832223/71d0e1da-66b2-11e6-9cb8-3983bcb81118.png)
+
+
 // TODO: Write a comment here explaining your choice
+
+//genre has many Author
+//author has many genre

f552f6f3b6f8b652da3002ae281eb4d061b1266a
RBK8 <maisaa.alkhedr@gmail.com>
2017-09-23 09:04:25 +0300

    Express

--- node-express-sequelize/index.js
+++ node-express-sequelize/index.js
@@ -2,6 +2,19 @@ var app = require('./server').app;
 var User = require('./server').User;
 var port = 3000;
 
+var express = require('express');
+var models  = require('../models');
+var router  = express.Router();
+
+router.get('/users',function(req, res){
+  models.User.findAll({ include : [User] })
+  .then ( users => {
+    console.log(JASON.stringfiy(users))
+  })
+}
+
+
+
 User.sync({ force: true })
   .then(function () {
     console.log('Users table created');
@@ -14,3 +27,4 @@ User.sync({ force: true })
     });
   });
   
+  module.exports = router ;

bd39ddebdd8101cb8c0136881c4b442a4c20cc2e
abeer-shami <abeer.shami887@gmail.com>
2017-09-23 09:42:54 +0300

    last commit

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,6 +11,8 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
+OPTION_5
+because the publisher has many others so the id in the publisher it shuld be a primary key in the authors and the id of the authors it will be a foriegn key in the publisher.
 
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice

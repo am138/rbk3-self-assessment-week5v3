10652100782ebe2c86ddb3721d38ab95df0fbbfc
AlaaRBK <alshyoukhi94@gmail.com>
2017-09-23 09:35:13 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -1,7 +1,7 @@
 var Schemas = {
 	
     
-    Option 5 (below) : becuse we connect many to many 
+    Option 6 (below) : becuse we connect many to many 
   
 };
 
@@ -9,13 +9,11 @@ var sqlPublisherAuthorSchema = CREATE TABLE sqlPublisherAuthorSchema({
 	id : varChar(20) NOT NULL  PRIMARY KEY,
 	firstName : varChar(255) NOT NULL,
     lastName : varChar(255) NOT NULL,
-    publisher : varChar(20) REFERENCES Permissions (sqlPublisherGenreSchema)
 })
 // TODO: Write a comment here explaining your choice
 
 var sqlPublisherGenreSchema = CREATE TABLE sqlPublisherGenreSchema({
 	id : varChar(20) NOT NULL  PRIMARY KEY,
-	authors : varChar(20) NOT NULL REFERENCES Permissions (sqlPublisherGenreSchema),
     name : varChar(20) NOT NULL 
 
 }) 

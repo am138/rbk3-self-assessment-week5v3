2c643b1568c03900daa8cc6f0402590e14bcf631
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-09-23 09:32:03 +0300

    Express

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -3,9 +3,19 @@ var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
 
+
+//define User table !!
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });
+
+app.get('/users' , function (req, res) {
+	//fetch all users from users table , using user model 
+	var data = User.findAll();
+
+	res.json(data);
+})
+
 
 module.exports = { 
   app: app,

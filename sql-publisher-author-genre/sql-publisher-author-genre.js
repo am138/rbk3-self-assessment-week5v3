c0b1ff5666adbf2ac49513369d080da205fd826d
shyma abed <noshima1000@gamil.com>
2017-09-23 09:41:53 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,11 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
-
+ //my chice coz we have one publisher so we dont need a publisher id in the authers table but to know authers we need thier id as a foriegn key
+//https://cloud.githubusercontent.com/assets/7968370/17832187/d9332c26-66b1-11e6-8e0b-a6b6ca21a871.png
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice
+//    https://cloud.githubusercontent.com/assets/7968370/17832210/4b42e176-66b2-11e6-8734-2e4081b5acf4.png
 
 var sqlAuthorGenreSchema = 
 // TODO: Write a comment here explaining your choice

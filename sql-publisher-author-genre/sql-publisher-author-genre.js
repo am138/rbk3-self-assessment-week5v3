01da27f9f4a0ef8e798789919cbe43073e940a52
areejmayadeh <engareej9r@gmail.com>
2017-09-23 09:09:08 +0300

    sql

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,13 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
+//if all cases in the read me related then (option 6) will be true since we have 1 to many relationship between authors and publishers and many to 1 between publisher and author ,then we need to create new table to this relation holds the primary key for each table 
 
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice
+//(option 4) because the relationship 1 to 1 between publisher and genre and 1 to many between genre and publisher. so we will set the primary key col  in genre as foriegn key in publisher 
+
 
 var sqlAuthorGenreSchema = 
 // TODO: Write a comment here explaining your choice
+//(option 6)  because we have 1 to many relationship between author and genre and many to 1 between author and genre ,so we need to create new table to this relation holds the primary key for each table 

0073b4ab90ccf43424a4fcce07c8ea530090c934
mazenalchalah <mazenalchalah@gmail.com>
2017-09-23 09:37:01 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,12 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
+//opthion_6:the relation between publisher and authors many to many because of that we put join table 
+//to break the relation between of them
 
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice
-
+//option_4:because the relation between publisher and genre many to one we put id_genre forighnkey in publisher table
 var sqlAuthorGenreSchema = 
 // TODO: Write a comment here explaining your choice
+// option6:because the relation between of author and gnre many to many because of that we put join table to break the relation between of them.

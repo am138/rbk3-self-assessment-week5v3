924d1f05ceb10d734cd74d89f764c27dab42e4ab
esraahusamia <esraa.hus93@yahoo.com>
2017-09-23 09:24:37 -1200

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,27 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
+// option 6 
+// the relation between publisher and auther is many to many 
+// so publisher publish to many auther and auther use many publisher to publish his work 
+// we create another table called publisher_auther has 
+// an id_publisher as forgin key and id_author as forgin key 
 
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice
+// option 4
+// the realtiom between publisher and genre is one to many 
+// each  publisher has one genre 
+//  the id_genre is forgin key in publisher table 
+
 
 var sqlAuthorGenreSchema = 
 // TODO: Write a comment here explaining your choice
+// option 6 
+// the relation between genre and auther is many to many 
+// so auther has many genre and genre has many auther 
+// we create another table called auther_genre has 
+// an id_genre as forgin key and id_author as forgin key 
+
+
+

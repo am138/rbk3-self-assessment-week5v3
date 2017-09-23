cc65a2efd19c92a457b49a4364db0244f7e265a4
jwanzerkani94 <zerkani-jwan@hotmail.com>
2017-09-23 09:45:00 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,13 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
+// option 6 because the relationship as mintioned is many to many 
+// "publisher has many authors and author has many publishers"
 
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice
+// option 4 because the relationship is one genre to many publishers so the genre id should be in the publisher table
 
 var sqlAuthorGenreSchema = 
 // TODO: Write a comment here explaining your choice
+// option 6 because the relationship is many genre to many authors so there should be a new table containing the id's

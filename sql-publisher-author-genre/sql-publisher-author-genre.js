3eaa6e077119bcced7c8d60f8f8b741f6c84a044
sam-you <saddam.jbawi@gmail.com>
2017-09-23 09:38:42 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -9,11 +9,16 @@ var Schemas = {
   OPTION_6: 'option 6'
 };
 
-var sqlPublisherAuthorSchema = 
+var sqlPublisherAuthorSchema = Schemas.OPTION_6;
 // TODO: Write a comment here explaining your choice
 
-var sqlPublisherGenreSchema = 
-// TODO: Write a comment here explaining your choice
+// bacause the relation between the two tables is many to many so i have to put third table to implent the relationship as on to many betwwen thr to tasbles and the third table :
 
-var sqlAuthorGenreSchema = 
+
+var sqlPublisherGenreSchema = Schemas.OPTION_4;
 // TODO: Write a comment here explaining your choice
+//because the relationship is one to many between the tables so we have to put the primary key of gere as foriegn key inside publishe table :why to do that i mean the relationship itself just to keep trak the Publisher which Genre have  and implement the databases mangment systems concepts 
+
+var sqlAuthorGenreSchema =Schemas.OPTION_6; 
+// TODO: Write a comment here explaining your choice
+// because the relation ship is many to many between the tables so i need a third table to broke it to two one to many relationships this table will contain the ids of the other tables 

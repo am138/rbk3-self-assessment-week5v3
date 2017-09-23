0b6841e6c6fc193ae14fcbc701e5db16e9ca5b32
atallah-salah <atallah_salah@outlook.com>
2017-09-23 09:51:00 +0300

    Express

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -3,6 +3,25 @@ var app = express();
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
 
+
+app.get('/',function(req,res){
+  if(req.url!=='/' && req.url!=='/users'){
+    res.writeHead(404,{'Concat-Type':'text/plain'})
+    res.end();
+  }else{
+    res.writeHead(200,{'Concat-Type':'text/plain'})
+    res.end();
+  }
+})
+
+app.post('/users',function(req,res){
+  sequelize().sync()
+  .then(User => {
+    User.get({
+      User:true
+    });
+});
+
 var User = sequelize.define('User', {
   username: Sequelize.STRING
 });

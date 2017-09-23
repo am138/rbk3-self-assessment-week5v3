5a8ae8a5037f7e17dfa706cadb02026f2ed43357
duhaali <dohaali415@gmail.com>
2017-09-23 09:43:33 +0300

    SQL

--- sql-publisher-author-genre/sql-publisher-author-genre.js
+++ sql-publisher-author-genre/sql-publisher-author-genre.js
@@ -11,9 +11,12 @@ var Schemas = {
 
 var sqlPublisherAuthorSchema = 
 // TODO: Write a comment here explaining your choice
+// i think it's OPTION_5: 'option 5' because the relationship is many to many 
+//that's mean  many authors has many Publishers and the primary key for both shod be as a forghn key in the tow table 
 
 var sqlPublisherGenreSchema = 
 // TODO: Write a comment here explaining your choice
-
+// the relationship is one to many (one genre have many Publishers)
+//the primary key of genre shod be as a foren in Publishers
 var sqlAuthorGenreSchema = 
 // TODO: Write a comment here explaining your choice
